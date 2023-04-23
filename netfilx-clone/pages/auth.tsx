4cf41517881c7ab29d1b98d87826ecd66600ffd4
parkcheolryeon <qkrcjffus@naver.com>
2023-04-23 16:04:35 +0900

    Input setting complete

--- netfilx-clone/pages/auth.tsx
+++ netfilx-clone/pages/auth.tsx
@@ -1,4 +1,11 @@
+import Input from "@/components/Input";
+import { useState } from "react";
+
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -8,7 +15,34 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Sing in</h2>
-            <div className="flex flex-col gap-4"></div>
+            <div className="flex flex-col gap-4">
+              <Input
+                label="Username"
+                onChange={(e: any) => {
+                  setEmail(e.target.value);
+                }}
+                id="name"
+                value={name}
+              />
+              <Input
+                label="Email"
+                onChange={(e: any) => {
+                  setEmail(e.target.value);
+                }}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Password"
+                onChange={(e: any) => {
+                  setPassword(e.target.value);
+                }}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </div>
           </div>
         </div>
       </div>
